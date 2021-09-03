8364c87207243f9b86f9dd66296b8a203b3877d3
beneko <behzadnekouei@gmail.com>
2021-09-03 19:16:02 +0200

    set background to the window

--- main.py
+++ main.py
@@ -3,12 +3,22 @@ pygame.init()
 
 # generate first window
 pygame.display.set_caption("Shooter Game")
-pygame.display.set_mode((1080, 720))
+screen = pygame.display.set_mode((1080, 720))
+
+# import background image
+background = pygame.image.load('assets/bg.jpg')
 
 running = True
 
 # run program while running is ture
 while running:
+
+    # set background image to the window
+    screen.blit(background, (0, -200))
+
+    # update screen
+    pygame.display.flip()
+
     # see if player close the window
     for event in pygame.event.get():
         # verify if event is closing the window
