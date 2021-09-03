1f94878628774268a9d4066ee752b56e0fd47b39
beneko <behzadnekouei@gmail.com>
2021-09-03 19:53:01 +0200

    player onject added to the scrren

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import pygame
+
+from player import Player
+
+
 pygame.init()
 
 # generate first window
@@ -8,6 +12,9 @@ screen = pygame.display.set_mode((1080, 720))
 # import background image
 background = pygame.image.load('assets/bg.jpg')
 
+# create a player object
+player = Player()
+
 running = True
 
 # run program while running is ture
@@ -15,6 +22,9 @@ while running:
 
     # set background image to the window
     screen.blit(background, (0, -200))
+
+    # charge the player Image
+    screen.blit(player.image, player.rect)
 
     # update screen
     pygame.display.flip()
