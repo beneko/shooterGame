59892cd410fd4eaf7ddefbe18dcbea0a4089ea11
beneko <behzadnekouei@gmail.com>
2021-09-04 12:44:24 +0200

    add method move,remove and rotate to projectile class

--- projectile.py
+++ projectile.py
@@ -5,9 +5,30 @@ class Projectile(pygame.sprite.Sprite):
 
     def __init__(self, player):
         super().__init__()
-        self.velocity = 2
+        self.player = player
+        self.velocity = 3
         self.image = pygame.image.load('assets/projectile.png')
+        # resize image
         self.image = pygame.transform.scale(self.image, (50, 50))
         self.rect = self.image.get_rect()
         self.rect.x = player.rect.x + 100
         self.rect.y = player.rect.y + 100
+        self.origin_image = self.image
+        self.angle = 0
+
+    def move(self):
+        self.rect.x += self.velocity
+        self.rotate()
+        # delete projectile when it is out of screen
+        if self.rect.x > 1080:
+            self.remove()
+
+    def remove(self):
+        self.player.all_projectiles.remove(self)
+
+    def rotate(self):
+        self.angle += 8
+        # rotate image by angle
+        self.image = pygame.transform.rotozoom(self.origin_image, self.angle, 1)
+        # rotation by center of image
+        self.rect = self.image.get_rect(center=self.rect.center)
