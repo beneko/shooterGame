a83bc42abce1acbcb8c8abf9d8f0867cd10af732
beneko <behzadnekouei@gmail.com>
2021-09-05 19:19:41 +0200

    method spawn_monster and check_collision added to class game

--- game.py
+++ game.py
@@ -1,9 +1,27 @@
+import pygame.sprite
+
 from player import Player
+from monster import Monster
 
 
 class Game:
 
     def __init__(self):
         # generate a player object
-        self.player = Player()
+        self.all_players = pygame.sprite.Group()
+        self.player = Player(self)
+        self.all_players.add(self.player)
         self.pressed = {}
+        # generate a group of monster object
+        self.all_monsters = pygame.sprite.Group()
+        self.spawn_monster()
+
+    def spawn_monster(self):
+        # generate a monster object
+        monster = Monster(self)
+        self.all_monsters.add(monster)
+
+    def check_collision(self, sprite, group):
+        return pygame.sprite.spritecollide(sprite, group, False, pygame.sprite.collide_mask)
+
+
