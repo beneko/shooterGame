3b7b9869b7ca06f4b1b21a413783573e0a3a7ec8
beneko <behzadnekouei@gmail.com>
2021-09-05 19:21:21 +0200

    let player to move just if there is not any collision with monsters

--- player.py
+++ player.py
@@ -1,11 +1,14 @@
 import pygame
+
+import game
 from projectile import Projectile
 
 
 class Player(pygame.sprite.Sprite):
 
-    def __init__(self):
+    def __init__(self, game):
         super().__init__()
+        self.game = game
         self.health = 100
         self.max_health = 100
         self.attack = 10
@@ -17,10 +20,14 @@ class Player(pygame.sprite.Sprite):
         self.all_projectiles = pygame.sprite.Group()
 
     def move_right(self):
-        self.rect.x += self.velocity
+        # verify if player isn't in collision with the monsters
+        if not self.game.check_collision(self, self.game.all_monsters):
+            self.rect.x += self.velocity
 
     def move_left(self):
-        self.rect.x -= self.velocity
+        # verify if player isn't in collision with the monsters
+        if not self.game.check_collision(self, self.game.all_monsters):
+            self.rect.x -= self.velocity
 
     def launch_projectile(self):
         # create a new projectile object and add it to the group
